effaa25a2bf612c675fbe8d27edcec68c7454341
SonomuraHiroyuki <abc332554331@gmail.com>
2020-11-15 11:02:53 +0900

    3.new,createを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def create
+    
+  end
+
+  private
+  def tank_params
+    params.require(:task).permit(:title, :content)
+  end
 end
