a9fb5a06f967cc0f17536a172b7842ec8443bcb7
Rinko Yabiku <wta15214@nuhw.ac.jp>
2020-11-24 19:47:00 +0900

    一覧表示機能indexを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
  
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
-  def CreateUsers
+  def create
     User.create(user_params)
   end
 
@@ -12,5 +16,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :age)
   end
-  
+
 end
